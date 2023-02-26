83a963c777945e674b38b8df7d580ed95fae69f1
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2023-02-26 18:28:29 +0000

    initial commit

---
+++ src/screens/ProductsScreen.js
@@ -0,0 +1,35 @@
+import { StyleSheet, Text, View, Image, FlatList } from "react-native";
+import products from "../data/products";
+import React from "react";
+
+const ProductsScreen = () => {
+  return (
+    <FlatList
+      data={products}
+      renderItem={({ item }) => (
+        <View style={styles.itemContainer}>
+          <Image
+            source={{
+              uri: item.image,
+            }}
+            style={styles.image}
+          />
+        </View>
+      )}
+      numColumns={2}
+    />
+  );
+};
+
+export default ProductsScreen;
+
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  itemContainer: {
+    width: "50%",
+    padding: 1,
+  },
+});
